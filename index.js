49b88ace7c8e7df568f2ab4f43c7ed274a8a930c
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2023-01-18 14:19:34 +0800

    first commit - lots of stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+import characterData from "./data.js";
+
+import Character from "./character.js";
+
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
+
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+}
+
+render();
